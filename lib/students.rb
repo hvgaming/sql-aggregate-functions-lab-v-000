a6fc6c46e01c9d4280bee35a4e55ad4413f36324
hugo valois <hvgaming@yahoo.com>
2018-11-06 02:03:48 +0000

    automatically backed up by learn

--- lib/students.rb
+++ lib/students.rb
@@ -9,11 +9,13 @@ def lowest_student_gpa
 end
 
 def average_student_gpa
-  
+  "SELECT AVG(students.gpa) FROM students;"
 end
 
 def total_tardies_for_all_students
+  
 end
 
 def average_gpa_for_9th_grade
+  
 end
